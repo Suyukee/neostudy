2a61aaf3ce19101d3e1abf8d21f68ef72a9bbd9c
Suyukee <diefuck@ya.ru>
2024-11-16 00:03:39 +0300

    Правки

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 const fullSlide = 256 + 3;
 const halfSlide = 128 + 3;
 
-let offset = 0;
+sliderInit('slider', fullSlide, halfSlide);
 
-const sliderLine = document.querySelector('.slider__line');
-const sliderPrev = document.querySelector('.nav__btn-prev');
-const sliderNext = document.querySelector('.nav__btn-next');
+function sliderInit(elementId, fullSlide, halfSlide) {
+	const slider = document.getElementById(elementId);
+	const sliderLine = slider.querySelector('.slider__line');
+	const sliderPrev = slider.querySelector('.nav__btn-prev');
+	const sliderNext = slider.querySelector('.nav__btn-next');
 
-sliderPrev.disabled = true;
+	let offset = 0;
+	sliderPrev.disabled = true;
 
-document.querySelector('.nav__btn-next').addEventListener('click', () => {
-	if (offset === 2 * fullSlide) {
-		offset += fullSlide + halfSlide;
-		sliderLine.style.left = -offset + 'px';
-		sliderPrev.disabled = false;
-		sliderNext.disabled = true;
-	} else {
-		offset += fullSlide;
-		sliderLine.style.left = -offset + 'px';
-		sliderPrev.disabled = false;
-	}
-});
+	document.querySelector('.nav__btn-next').addEventListener('click', () => {
+		if (offset === 2 * fullSlide) {
+			offset += fullSlide + halfSlide;
+			sliderLine.style.left = -offset + 'px';
+			sliderPrev.disabled = false;
+			sliderNext.disabled = true;
+		} else {
+			offset += fullSlide;
+			sliderLine.style.left = -offset + 'px';
+			sliderPrev.disabled = false;
+		}
+	});
 
-document.querySelector('.nav__btn-prev').addEventListener('click', () => {
-	if (offset === 3 * fullSlide + halfSlide) {
-		offset -= fullSlide + halfSlide;
-		sliderLine.style.left = -offset + 'px';
-		sliderNext.disabled = false;
-	} else if (offset === fullSlide) {
-		offset -= fullSlide;
-		sliderLine.style.left = -offset + 'px';
-		sliderPrev.disabled = true;
-	} else {
-		offset -= fullSlide;
-		sliderLine.style.left = -offset + 'px';
-		sliderPrev.disabled = false;
-	}
-});
+	document.querySelector('.nav__btn-prev').addEventListener('click', () => {
+		if (offset === 3 * fullSlide + halfSlide) {
+			offset -= fullSlide + halfSlide;
+			sliderLine.style.left = -offset + 'px';
+			sliderNext.disabled = false;
+		} else if (offset === fullSlide) {
+			offset -= fullSlide;
+			sliderLine.style.left = -offset + 'px';
+			sliderPrev.disabled = true;
+		} else {
+			offset -= fullSlide;
+			sliderLine.style.left = -offset + 'px';
+			sliderPrev.disabled = false;
+		}
+	});
+}
