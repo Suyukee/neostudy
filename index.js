fbd06dcf7d7427926d515d997c090374fc4f5e51
Kozyrev Vladimir <39066019+Suyukee@users.noreply.github.com>
2024-11-15 17:22:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let button = document.querySelector('.main__btn-up');
+
+window.onscroll = () => {
+	scrollCheck();
+};
+
+const scrollCheck = () => {
+	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+		button.style.display = 'flex';
+	} else {
+		button.style.display = 'none';
+	}
+};
+
+const scrollUp = () => {
+	document.documentElement.scrollTop = 0;
+};
