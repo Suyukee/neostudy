508e0b2cdcf1fb9e451c95f96169cb14772509b7
Suyukee <diefuck@ya.ru>
2024-11-15 18:33:49 +0300

    Третье задание

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function filter() {
+	const filter = document.querySelector('.main__input').value.toLowerCase();
+	const list = document.querySelector('.user-list');
+	const li = list.getElementsByTagName('li');
 
+	for (i = 0; i < li.length; i++) {
+		const txtValue = li[i].textContent.toLowerCase();
+
+		if (txtValue.indexOf(filter) !== -1) {
+			li[i].style.display = '';
+		} else {
+			li[i].style.display = 'none';
+		}
+	}
+}
